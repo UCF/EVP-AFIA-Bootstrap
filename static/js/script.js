af2b337eddf93aae19ba4590369d61bd4fff4461
Jo Greybill <jogreybill@gmail.com>
2013-02-19 11:11:46 -0500

    Make script.js ACTUALLY include the webcom-base.js stuff it needs

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,26 @@
-$().ready(function() {
-	
-	$('.select-links')
-		.change(function() {
-			var selected = $(this).children('option:selected');
-			if(typeof selected != 'undefined') {
-				var url = selected.val();
-				if(url != '') {
-					window.location = url;
+if (typeof jQuery != 'undefined'){
+	jQuery(document).ready(function($) {
+		Webcom.slideshow($);
+		Webcom.analytics($);
+		Webcom.handleExternalLinks($);
+		Webcom.loadMoreSearchResults($);
+		
+		$('.select-links')
+			.change(function() {
+				var selected = $(this).children('option:selected');
+				if(typeof selected != 'undefined') {
+					var url = selected.val();
+					if(url != '') {
+						window.location = url;
+					}
 				}
-			}
-		});
-	$('#home-images-carousel').carousel('cycle');
-	
-	$('.menu li:last-child').addClass('last');
-	if ($.browser.msie && $.browser.version < 9) {
-		$('.menu li').append('<span class="ieseparator">•</span>');
-		$('.menu li.last .ieseparator').remove();
-	}
-});
+			});
+		$('#home-images-carousel').carousel('cycle');
+		
+		$('.menu li:last-child').addClass('last');
+		if ($.browser.msie && $.browser.version < 9) {
+			$('.menu li').append('<span class="ieseparator">•</span>');
+			$('.menu li.last .ieseparator').remove();
+		}
+	});
+}else{console.log('jQuery dependancy failed to load');}
