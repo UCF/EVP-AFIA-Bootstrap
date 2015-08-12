5879872c033acc7789b6cb79afe5af97779e0ad7
Jim Barnes <james.barnes@ucf.edu>
2015-08-12 09:08:02 -0400

    Replaced page specific template for appeals and added json template.

---
+++ template-json-only.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: JSON Only
+ **/
+disallow_direct_load('template-one-column.php');
+the_post();
+
+if ( $post ) {
+
+	$stylesheet_id = get_post_meta( $post->ID, 'page_stylesheet', True );
+	$stylesheet_url = '';
+	if ( $stylesheet_id !== False ) {
+		$stylesheet_url = wp_get_attachment_url( $stylesheet_id );
+	}
+
+	$json = array(
+		'title' => $post->post_title,
+		'content' => wpautop($post->post_content),
+		'stylesheet' => $stylesheet_url
+	);
+
+	header('Content-Type:application/json');
+
+	echo json_encode($json);
+}
+
+?>
